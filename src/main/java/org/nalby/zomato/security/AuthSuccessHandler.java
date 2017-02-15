e766b759facb5b145aa4bb4b430b4a05af461f10
Connor Lin <connor.lin@lumiit.com.au>
2017-02-15 18:14:27 +1100

    make code clean.

--- src/main/java/org/nalby/zomato/security/AuthSuccessHandler.java
+++ src/main/java/org/nalby/zomato/security/AuthSuccessHandler.java
@@ -13,24 +13,41 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 import org.springframework.stereotype.Component;
+
+import com.fasterxml.jackson.core.JsonGenerationException;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Component("authSuccessHandler")
-public class AuthSuccessHandler extends SavedRequestAwareAuthenticationSuccessHandler {
+public class AuthSuccessHandler extends SavedRequestAwareAuthenticationSuccessHandler implements LogoutSuccessHandler {
 
 	private final ObjectMapper objectMapper = new ObjectMapper();
 	private final static Logger logger = LoggerFactory.getLogger(AuthSuccessHandler.class);
+	
+	
+	private void writeResponse(ErrorCode code, HttpServletResponse response, int httpCode) throws JsonGenerationException, JsonMappingException, IOException {
+		Response response2 = new Response(ErrorCode.EOK);
+		response.setStatus(HttpServletResponse.SC_OK);
+		PrintWriter writer = response.getWriter();
+		objectMapper.writeValue(writer, response2);
+		writer.flush();
+	}
 
 	@Override
 	public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
 			Authentication authentication) throws IOException, ServletException {
-		response.setStatus(HttpServletResponse.SC_OK);
-		Response response2 = new Response(ErrorCode.EOK);
 		logger.info("{} logged in.", authentication.getName());
-		PrintWriter writer = response.getWriter();
-		objectMapper.writeValue(writer, response2);
-		writer.flush();
+		writeResponse(ErrorCode.EOK, response, HttpServletResponse.SC_OK);
+	}
+	
+	public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication)
+			throws IOException, ServletException {
+		if (authentication != null) {
+			logger.info("{} loged out.", authentication.getName());
+		}
+		writeResponse(ErrorCode.EOK, response, HttpServletResponse.SC_OK);
 	}
 }
 
